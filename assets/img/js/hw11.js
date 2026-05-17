e1429d63a281e1d7a43f38fe98b8869c80e74ba8
eunwoo <ugim44899@snu.ac.kr>
2026-05-17 21:51:57 +0000

    hw11

---
+++ assets/img/js/hw11.js
@@ -0,0 +1,32 @@
+// [숙제11] 동적 웹 페이지 구현
+// 2025-13198 김은우
+
+// Q1
+const themeBtn = document.querySelector('#theme-btn');
+const q1Box = document.querySelector('#q1-box');
+themeBtn.addEventListener('click', () => { 
+    q1Box.classList.toggle('dark');
+    if (q1Box.classList.contains('dark')) {
+        themeBtn.textContent = '라이트 모드';
+    } else {
+        themeBtn.textContent = '다크 몯';
+    }   
+});
+
+// Q2
+const input = document.querySelector('#q2-input');
+const count = document.querySelector('#q2-count');
+const warn = document.querySelector('#q2-warn');
+
+input.addEventListener('input', (e) => { 
+    const currentLength = e.target.value.length;
+    count.textContent = currentLength;
+
+    if (currentLength > 100) {
+        warn.textContent = '100자를 넘었습니다.';
+        warn.style.color = 'crimson';
+    } else {
+        warn.textContent = '';
+    }
+});
+
