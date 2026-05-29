fffee7ae3fbcf9ee1e8950b1dcadc75e8d8dd859
eunwoo <ugim44899@snu.ac.kr>
2026-05-29 03:49:51 +0000

    hw12

---
+++ assets/img/js/compare.js
@@ -0,0 +1,70 @@
+function extractBody(text) {
+    const startMark = "*** START OF THE PROJECT GUTENBERG EBOOK";
+    const endMark   = "*** END OF THE PROJECT GUTENBERG EBOOK";
+
+    const startIdx = text.indexOf(startMark);
+    const endIdx   = text.indexOf(endMark);
+
+    // 시작 표시 다음 줄부터 끝 표시 직전까지
+    return text.slice(startIdx, endIdx);
+}
+
+// 2. 가져온 본문에서 단어들의 배열을 넣기
+function getWords(text) {
+    return text
+        .toLowerCase()
+        .replace(/[.,!?;:'"‘’“”()\[\]_*]/g, " ")
+        .split(/\s+/)
+        .filter(w => w.length > 0);
+}
+
+// 계속
+// 단어들의 배열에서 불용어를 제거하기
+function removeStopwords(words, stopwords) {
+    return words.filter(w => !stopwords.includes(w));
+}
+
+function topN(counts, n) { // counts: 객체
+    return Object.entries(counts) // 객체 -> 배열로 전환
+        .sort((a, b) => b[1] - a[1]) // 빈도수 내림차순 정렬
+        .slice(0, n) // 상위 n개 선택
+}
+
+
+function countWords(words) {
+    const counts = {}; // 빈 객체 초기화
+    for (const word of words) {
+        counts[word] = (counts[word] || 0) + 1;
+    }
+    return counts;
+}
+
+
+function analyze(text, stopwords) {
+    const body = extractBody(text);
+    const words = getWords(body);
+    const cleaned = removeStopwords(words, stopwords);
+    const counts = countWords(cleaned);
+    return topN(counts, 30);
+}
+
+
+function topN(counts, n) { // counts: 객체
+    return Object.entries(counts) // 객체 -> 배열로 전환
+        .sort((a, b) => b[1] - a[1]) // 빈도수 내림차순 정렬
+        .slice(0, n) // 상위 n개 선택
+}
+Promise.all([
+    fetch("/data/scarlet.txt").then(r => r.text()),
+    fetch("/data/hound.txt").then(r => r.text()),
+    fetch("/data/stopwords-en.txt").then(r => r.text()),
+]).then(([scarletText, houndText, stopwordsText]) => {
+    const stopwords = stopwordsText.split(/\s+/)
+    .filter(w => w.length > 0);
+    const scarletTop = analyze(scarletText, stopwords);
+    const houndTop = analyze(houndText, stopwords);
+    drawChart("#chart-scarlet", scarletTop, "rgba(220, 53, 69, 0.6)");
+    drawChart("#chart-hound", houndTop, "rgba(54, 162, 235, 0.6)");
+});
+
+// 종합: text ---> 상위 n개 
